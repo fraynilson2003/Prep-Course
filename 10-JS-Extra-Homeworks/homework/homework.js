72c22ee5fdf59e5e75bf7b1f6de16b4241c8cd32
fraynilson2003 <fraynilson2003@gmail.com>
2022-10-22 11:17:09 -0500

    homework EXTRAS

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,14 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+  arNu = [];
+
+  for (var clave in objeto){
+    arNu.push([clave,objeto[clave]])
+  }
+
+  return arNu;
 }
 
 
@@ -18,16 +26,41 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-}
 
+  var objeto = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if(objeto.hasOwnProperty(string[i])){
+      objeto[string[i]] = objeto[string[i]] +1;
+    }else{
+      objeto[string[i]] = 1;
+    }
+    
+  }
+  return objeto;
+
+}
+console.log(numberOfCharacters("aaarreeett"))
 
 function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  let min = "";
+  let may = "";
+  for (let i = 0; i < s.length; i++) {
+    if(s[i] == s[i].toUpperCase()){
+      may = may+s[i];
+    }else{
+      min= min+s[i];
+    }   
+  }
+  return may+min;
+
 }
 
+console.log(capToFront("soyHENRY"))
 
 function asAmirror(str) {
   //La función recibe una frase. 
@@ -35,7 +68,14 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  let invert = str.split(" ")
+  let arrayFi = invert.map(function(ele){
+    return ele.split("").reverse().join("");
+  })
+  return arrayFi.join(" ")
+
 } 
+
 
 
 function capicua(numero){
@@ -43,6 +83,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+
+  if (numero == Number(numero.toString().split("").reverse().join(""))){
+    return "Es capicua";
+  }else{
+    return "No es capicua";
+  }
+
 }
 
 
@@ -50,15 +97,34 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var newCadena = "";
+  for (let i = 0; i < cadena.length; i++) {
+    if(cadena[i].toLowerCase() == "a" || cadena[i].toLowerCase() == "b" || cadena[i].toLowerCase() == "c" ){  
+
+    }else{
+      newCadena = newCadena + cadena[i]; 
+    }
+  }
+  return newCadena;
 }
+
+console.log(deleteAbc("Jorgeasidobebece"))
 
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+  var nuevoArray = arr.sort(function (a, b) {
+    return a.length - b.length
+  })
+  return nuevoArray;
+  
 }
 
+var g = ["You", "are", "beautiful", "looking"]
+console.log(sortArray(g))
 
 function buscoInterseccion(arreglo1, arreglo2){
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
@@ -66,9 +132,22 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+
+  var nuevoArreglo = [];
+
+  for (let i = 0; i < arreglo1.length; i++) {
+    for (let e = 0; e < arreglo2.length; e++) {
+      if(arreglo1[i] == arreglo2 [e]){
+        nuevoArreglo.push(arreglo1[i]);
+      }
+    }
+  }
+  return nuevoArreglo;
 }
 
-
+var dw = [4,5,3,13]
+var de = [4,9,6,2,3,1,5]
+console.log(buscoInterseccion(dw,de))
 
 // No modificar nada debajo de esta línea
 // --------------------------------
